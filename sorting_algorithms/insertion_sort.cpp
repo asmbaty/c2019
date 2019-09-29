8c8725d54ef0b44e49d1dedd58970dc516945c34
Aharon Smbatyan <aharon.smbatyan@gmail.com>
2019-09-29 15:45:46 +0400

    Insertion sort

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,25 @@
+// Inserting sort
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void insertion_sort(std::vector<int>& arr)
+{
+    for(auto iter = arr.begin(); iter != arr.end(); ++iter) {
+        auto min = std::min_element(iter, arr.end());
+        static_assert( std::is_same<decltype(iter), decltype(min)>::value);
+        std::swap(*iter, *min);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> arr {5, 2, 3, 4, 1};
+    insertion_sort(arr);
+    for(const auto& elem : arr) {
+        std::cout << elem << ' ';
+    }
+    std::cout << std::endl;
+    
+    return EXIT_SUCCESS;
+}
